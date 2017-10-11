f6d689c52cd770726f171bcda6a14db91261f6dd
lgj <2086530297@qq.com>
2017-10-11 16:19:42 +0800

    Create sublime 配置 Terminal cmder.sh

---
+++ Python/sublime 配置 Terminal cmder.sh
@@ -0,0 +1,10 @@
+#sublime 配置 Terminal cmder
+在 packagecontrol.io 可以找到 Terminal 
+在 cmder.net 下载 cmder
+复制 Terminal.sublime-settings 文件到 C:\Users\WXG\AppData\Roaming\Sublime Text 3\Packages\User 目录下. 作如下修改:
+{
+    "terminal": "C:/wxg/tools/cmder_mini/Cmder.exe",
+    "parameters": ["/START", "%CWD%"]
+}
+然后就可以在sublime下调用cmder了.
+如果加上 "parameters": ["/START", "%CWD%"] -> 可以定位到当前打开文件所在的目录.
