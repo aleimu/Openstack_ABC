ff3b448933aefc49f4aaa25ec5ff2c4b8299d76c
lgj <2086530297@qq.com>
2017-10-14 22:17:09 +0800

    Create 学习资源.sh

---
+++ Openstack/学习资源.sh
@@ -0,0 +1,4 @@
+http://www.cnblogs.com/junneyang/p/5257303.html
+http://www.cnblogs.com/littlebugfish/p/4660586.html
+http://blog.csdn.net/weixin_39400463/article/details/76407950
+https://github.com/int32bit/openstack-workflow
