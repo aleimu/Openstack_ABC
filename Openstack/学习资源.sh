e9126d07ec9b3837c2cfec8a980a3d85bd47bef3
lgj <2086530297@qq.com>
2017-10-30 23:01:39 +0800

    Update 学习资源.sh

--- Openstack/学习资源.sh
+++ Openstack/学习资源.sh
@@ -2,3 +2,9 @@ http://www.cnblogs.com/junneyang/p/5257303.html
 http://www.cnblogs.com/littlebugfish/p/4660586.html
 http://blog.csdn.net/weixin_39400463/article/details/76407950
 https://github.com/int32bit/openstack-workflow
+
+
+#pycharm快捷键、常用设置、配置管理
+http://blog.csdn.net/pipisorry/article/details/50924395
+http://blog.csdn.net/pipisorry/article/details/50924395
+
