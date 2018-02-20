bed8a4cc8272ad728109ca610ae489926145f039
lgj <2086530297@qq.com>
2018-02-20 21:03:44 +0800

    Update 面试题2.sh

--- Others/面试题2.sh
+++ Others/面试题2.sh
@@ -103,4 +103,37 @@ if __name__=='__main__':
     print binarySearch(l, 13)
     print binarySearch(l, 444)
     
+4.广度遍历和深度遍历二叉树
+## 14 二叉树节点
+class Node(object):
+    def __init__(self, data, left=None, right=None):
+        self.data = data
+        self.left = left
+        self.right = right
+ 
+tree = Node(1, Node(3, Node(7, Node(0)), Node(6)), Node(2, Node(5), Node(4)))
+ 
+## 15 层次遍历
+def lookup(root):
+    stack = [root]
+    while stack:
+        current = stack.pop(0)
+        print current.data
+        if current.left:
+            stack.append(current.left)
+        if current.right:
+            stack.append(current.right)
+## 16 深度遍历
+def deep(root):
+    if not root:
+        return
+    print root.data
+    deep(root.left)
+    deep(root.right)
+ 
+if __name__ == '__main__':
+    lookup(tree)
+    deep(tree)
+
+
 
